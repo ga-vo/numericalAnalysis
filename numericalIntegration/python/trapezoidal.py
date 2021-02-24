0cd969d0ca92d62623da38d24e5da9d837d147f6
ga-vo <78771070+ga-vo@users.noreply.github.com>
2021-02-23 22:08:17 -0500

    Add file trapezoidal.py
    Python function for trapezoidal rule for integration

---
+++ numericalIntegration/python/trapezoidal.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme(style='darkgrid')
+
+"""
+Parameters
+----------
+fun : function
+    Function on which the integration will be performed
+a : double
+    Lower limit
+b : double
+    Upper limit
+N : int
+    Number of equally spaced panels
+verbose: bool, default=False
+    If true print each iteration
+graph: bool, default=True
+    If true graphically shows the integration process
+"""
+def trapezoidal_integration_rule(fun, a, b, N, verbose=False, graph=True):
+    dx = (b-a)/N
+    x_k = a
+    intgrl = 0
+    if(verbose):
+        print("\n Integral between {0} and {1} \n dx= {2}".format(a, b, dx))
+        
+    for _ in range(1, N+1):
+        x_k_1 = x_k
+        x_k += dx
+        intgrl += fun(x_k_1) + fun(x_k)
+
+        if(verbose):
+            intgrlaux = intgrl*dx/2
+            print(
+                "\n x_k-1: {0}, x_k: {1}, integral: {2}".format(x_k_1, x_k, intgrlaux))
+        if(graph):
+            plt.fill_between([x_k_1,x_k], [fun(x_k_1), fun(x_k)], alpha=0.5)
+ 
+    intgrl *= dx/2
+    if(graph):
+        x=np.linspace(a,b,300)
+        f=fun(x)
+        plt.plot(x,f)
+        plt.show()
+
+    return intgrl
+    
