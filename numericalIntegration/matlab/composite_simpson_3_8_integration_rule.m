aaa7d8b9125756718d0434c3e8711e13e96eeaf9
ga-vo <78771070+ga-vo@users.noreply.github.com>
2021-02-26 23:20:27 -0500

    Update composite_simpson_3_8_integration_rule.m

--- numericalIntegration/matlab/composite_simpson_3_8_integration_rule.m
+++ numericalIntegration/matlab/composite_simpson_3_8_integration_rule.m
@@ -5,8 +5,10 @@ function intgrl = composite_simpson_3_8_integration_rule(fun,a,b,N)
     h=(b-a)./N;
     intgrl=fun(a);
     for k=1:M-1
-        x_k=a + k.*h;
-        intgrl = intgrl + 3.*fun(x_k);
+        if mod(N,3) ~= 0
+            x_k=a + k.*h;
+            intgrl = intgrl + 3.*fun(x_k);
+        end
     end
     for k=1:(M./3)-1
         x_3k= a + (3.*k.*h);
